9fabe8b44591998e9a807827e3aa456eff9a6822
Yannick Kinsbergen <ykinsbergen@gofilex.nl>
2021-10-28 23:49:14 +0200

    build foundation

---
+++ README.md
@@ -0,0 +1,97 @@
+# Commands
+>!help
+
+## Set voice
+>!voice [speaker] [speed]
+
+For example:
+>!voice Damayanti
+
+or
+
+>!voice bad_news 0.69
+
+## Get current voice
+>!currentvoice
+
+## Set a random voice
+>!randomvoice
+
+# List of Available Voices
+>!voices
+
+- Agnes (en_US)
+- Albert (en_US)
+- Alex (en_US)
+- Alice (it_IT)
+- Alva (sv_SE)
+- Amelie (fr_CA)
+- Anna (de_DE)
+- Bad_News (en_US)
+- Bahh (en_US)
+- Bells (en_US)
+- Boing (en_US)
+- Bruce (en_US)
+- Bubbles (en_US)
+- Carmit (he_IL)
+- Cellos (en_US)
+- Claire (nl_NL)
+- Damayanti (id_ID)
+- Daniel (en_GB)
+- Deranged (en_US)
+- Diego (es_AR)
+- Ellen (nl_BE)
+- Felipe (pt_BR)
+- Fiona (en-scotland)
+- Fred (en_US)
+- Good_News (en_US)
+- Hysterical (en_US)
+- Ioana (ro_RO)
+- Joana (pt_PT)
+- Jorge (es_ES)
+- Juan (es_MX)
+- Junior (en_US)
+- Kanya (th_TH)
+- Karen (en_AU)
+- Kate (en_GB)
+- Kathy (en_US)
+- Kyoko (ja_JP)
+- Laura (sk_SK)
+- Lee (en_AU)
+- Lekha (hi_IN)
+- Luca (it_IT)
+- Luciana (pt_BR)
+- Maged (ar_SA)
+- Mariska (hu_HU)
+- Markus (de_DE)
+- Mei-Jia (zh_TW)
+- Melina (el_GR)
+- Milena (ru_RU)
+- Moira (en_IE)
+- Monica (es_ES)
+- Nora (nb_NO)
+- Oliver (en_GB)
+- Paulina (es_MX)
+- Princess (en_US)
+- Ralph (en_US)
+- Rishi (en_IN)
+- Samantha (en_US)
+- Sara (da_DK)
+- Satu (fi_FI)
+- Serena (en_GB)
+- Sin-ji (zh_HK)
+- Tessa (en_ZA)
+- Thomas (fr_FR)
+- Ting-Ting (zh_CN)
+- Trinoids (en_US)
+- Veena (en_IN)
+- Vicki (en_US)
+- Victoria (en_US)
+- Whisper (en_US)
+- Xander (nl_NL)
+- Yelda (tr_TR)
+- Yuna (ko_KR)
+- Yuri (ru_RU)
+- Zarvox (en_US)
+- Zosia (pl_PL)
+- Zuzana (cs_CZ)
---
+++ index.js
@@ -0,0 +1,200 @@
+const tmi = require("tmi.js");
+const Say = require( "say" ).Say;
+const say = new Say( "darwin" );
+
+const fs = require( "fs" );
+
+const { username, password, channels } = require( "./config.json" );
+const { voices, rates } = require( "./data.js" );
+const preferencesByUserId = require( "./userPreferences.json");
+
+const localesBySpeaker = new Map();
+for ( const { speaker, locale } of voices ) {
+  localesBySpeaker.set( speaker.toLowerCase(), locale );
+}
+
+const speakerList = voices.map( ( { speaker, locale } ) => {
+  return `${ speaker } (${locale})`;
+} );
+
+// define configuration options
+const opts = {
+  identity: {
+    username,
+    password,
+  },
+  channels,
+};
+
+// prepending messages with this string allows us to control the volume (thank
+// god we finally managed to find someone mentioning this)
+const volume = "[[volm 0.8]] ";
+
+const randomElement = ( array, key ) => {
+  if ( key ) {
+    return array[ Math.floor( Math.random() * array.length ) ][ key ];
+  }
+
+  return array[ Math.floor( Math.random() * array.length ) ];
+};
+
+// create a client with our options
+const client = new tmi.client( opts );
+
+client.connect();
+client.on( "connected", ( addr, port ) => {
+  console.log( `* Connected to ${addr}:${port}` );
+} );
+
+let speaking = false;
+let queue = [];
+const speak = ( message, userId, speaker ) => {
+  let voice = speaker;
+  let rate = 1;
+  if ( userId ) {
+    ( { voice = "alex", rate = 1 } = preferencesByUserId[ userId ] || {} );
+  }
+
+  if ( message ) {
+    let _voice = voice;
+    if ( _voice.includes( "_" ) ) {
+      _voice = _voice.replace( "_", " " );
+    }
+
+    queue.push( { message, voice: _voice, rate } );
+  }
+
+  if ( speaking ) {
+    return;
+  }
+
+  if ( !speaking ) {
+    speaking = true;
+    say.speak( ( volume + queue[ 0 ].message ), queue[ 0 ].voice, queue[ 0 ].rate, ( err ) => {
+      if ( err ) {
+        console.error( err );
+      }
+
+      speaking = false;
+      queue.shift();
+
+      if ( queue.length ) {
+        setTimeout( () => {
+          speak();
+        }, 500 );
+      }
+    } );
+  }
+}
+
+const saveUserPreference = ( userId, preference ) => {
+  if ( !preference || ( typeof preference !== "object" ) || Array.isArray( preference ) ) {
+    return;
+  }
+
+  preferencesByUserId[ userId ] = { ...preference };
+
+  // store user preferences in a local file, so that if the bot is restarted
+  // they can get their last-assigned voices again
+  fs.writeFileSync( "./userPreferences.json", JSON.stringify( preferencesByUserId, null, 2 ) );
+}
+
+// called every time a message is sent in the chat
+client.on( "message", ( target, context, message, self ) => {
+  // ignore messages from the bot itself
+  if ( self ) {
+    return;
+  }
+
+  const { "user-id": userId } = context;
+  if ( !userId ) {
+    return;
+  }
+
+  if ( !preferencesByUserId[ userId ] ) {
+    saveUserPreference( userId, { voice: randomElement( voices, "speaker" ), rate: randomElement( rates ) } );
+  }
+
+  message = message.trim().toLowerCase();
+
+  if ( message === "!maybetta" ) {
+    speak( "5 5 5 5 5", null, "luca" );
+    return;
+  }
+
+  if ( message === "!voices" ) {
+    // list all voices and their locales
+    client.say( target, speakerList.join( ", " ) );
+    return;
+  }
+
+  if ( message === "!voice" ) {
+    client.say( target, `@${ context[ "display-name" ] } Use "!voice [speaker|locale] [speed]" to set your voice, see "!voices" for the full list FrankerZ` );
+    return;
+  }
+
+  if ( ( message === "!help" ) || ( message === "!voice help" ) || ( message ==="!tts" ) ) {
+    client.say( target, `@${ context[ "display-name" ] } "!voice [speaker|locale] [speed]" | "!currentvoice" | "!randomvoice" | "!voices" LilZ` );
+    return;
+  }
+
+  if ( ( message === "!currentvoice" ) || ( message === "!cv" ) ) {
+    // have the bot reply with the user's current voice
+    const { voice, rate } = preferencesByUserId[ userId ];
+    client.say( target, `@${ context[ "display-name" ] } Current voice: "${ voice } (${ localesBySpeaker.get( voice.toLowerCase() ) })" at ${ rate }x speed.` );
+    return;
+  }
+
+  if ( ( message === "!voice random" ) || ( message === "!randomvoice" ) || ( message === "!random" ) || ( message === "!rv" ) ) {
+    // give the user a random voice
+    const getRandomVoice = ( voice ) => {
+      const _voice = randomElement( voices, "speaker" );
+      if ( _voice === voice ) {
+        getRandomVoice( voice );
+      }
+      else {
+        return _voice;
+      }
+    }
+    const voice = randomElement( voices, "speaker" );
+    const rate = randomElement( rates );
+
+    preferencesByUserId[ userId ] = { voice, rate };
+
+    saveUserPreference( userId, { voice, rate } );
+    speak( `${ context[ "display-name" ] } set their voice to ${ voice }`, userId );
+    return;
+  }
+  else if ( message.startsWith( "!voice" ) ) {
+    let [ , voice, rate = 1 ] = message.split( /\s/ );
+    if ( !voice || isNaN( rate ) ) {
+      return;
+    }
+
+    if ( rate < 0.1 ) {
+      rate = 0.1;
+    }
+    else if ( rate > 10 ) {
+      rate = 10;
+    }
+
+    const matchedVoice = voices.find( ( { speaker, locale } ) => {
+      if ( ( speaker.toLowerCase() === voice ) || ( locale.toLowerCase() === voice ) ) {
+        // try to match both speaker names and their given locales; for locales
+        // just pick the first voice we can find
+        return true;
+      }
+    } );
+
+    if ( !matchedVoice || ( matchedVoice === -1 ) ) {
+      client.say( target, `@${ context[ "display-name" ] } I don't know what voice that is FrankerZ` );
+      return;
+    }
+
+    saveUserPreference( userId, { voice: matchedVoice.speaker, rate } );
+    speak( `${ context[ "display-name" ] } set their voice to ${ matchedVoice.speaker }`, userId );
+    return;
+  }
+
+  speak( message, userId );
+} );
